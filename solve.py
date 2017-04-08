a98143b84bbab00f88efee8990c84c6bb5fcfc17
AlineKl <al.kladieva@gmail.com>
2017-04-08 15:04:02 +0300

    pure literal elimination added

--- solve.py
+++ solve.py
@@ -2,7 +2,7 @@
 
 import sys
 import signal
-import random
+import itertools
 
 TRUE = 1
 FALSE = -1
@@ -11,27 +11,51 @@ UNKNOWN = 0
 f = open(sys.argv[1])
 lines = f.readlines()
 f.close()
+
+# parsing
 start = int()
 for i in range(len(lines)):
     if lines[i][0] == 'p':
         start = i
         break
+
 words = lines[start].split()
 nbvar, nbclauses = int(words[2]), int(words[3])
 clauses = [[int(x) for x in lines[i].split()[:-1]] for i in range(start + 1, start + 1 + nbclauses)]
-
 # parsing finished
 
 variables = [UNKNOWN] * nbvar
 variables_forced = [False] * nbvar
-stack = []
+stack = [] 
+
+merged = list(itertools.chain.from_iterable(clauses))
+unique = set(merged)
 
 
-def assign(var_number, value=None):
+def get_sign(x):
+    if x > 0:
+        return 1
+    else:
+        return -1
+
+
+def remove_pure(clauses):
+    unassigned_variables = [v for v in unique if variables[v * get_sign(v) - 1] == UNKNOWN]
+
+    for num in unassigned_variables:
+        if (-num not in unassigned_variables):
+            sign = get_sign(num)
+            num *= sign
+            assign(num - 1, sign)
+
+
+def assign(var_number, value = None):
     if not 0 <= var_number < len(variables):
         raise ValueError("var_number is out of bounds")
+
     if variables[var_number] != UNKNOWN and value is None:
         raise ValueError("already assigned")
+
     if value is None:
         variables[var_number] = TRUE
     else:  # forced assignment
@@ -39,26 +63,30 @@ def assign(var_number, value=None):
             raise ValueError("value is out of bounds")
         variables[var_number] = value
         variables_forced[var_number] = True
+
     stack.append(var_number)
 
 
 def resolve_conflict():
     if len(stack) == 0:
         return False
+
     flag = False
+
     while len(stack) > 0:
         last = stack.pop()
-        if variables_forced[last]:
+        if variables_forced[last]: 
             variables_forced[last] = False
             variables[last] = UNKNOWN
         else:
             flag = True
             break
+
     if flag:
-        assign(last, variables[last] * -1)
+        assign(last, variables[last] * -1)  # swap variable
         return True
     else:
-        return False
+        return False  # all variables were forced, nothing to swap, conflict cannot be resolved
 
 
 var_changed = bool()
@@ -66,42 +94,49 @@ var_changed = bool()
 
 def clause_satisfied(clause):
     unknown_counter = 0
-    for num in clause:
+
+    for num in clause:  # num = number of variable (1 = x1, 2 = x2, ..)
         if num == 0:
             raise ValueError("clause cannot contain zeros")
-        if num > 0:
-            sign = 1
-        else:
-            sign = -1
-        num *= sign   # make num positive
-        if variables[num - 1] == sign:
-            return TRUE
+        sign = get_sign(num)
+        num *= sign   # make num positive to serve as an index
+        if variables[num - 1] == sign:  # if variable №num is assigned and it is TRUE
+            return TRUE  # than the whole clause is satisfied (cause it's disjunction)
+
         if variables[num - 1] == UNKNOWN:
             unknown_counter += 1
             last_unknown = num - 1
             last_unknown_sign = sign
-    if unknown_counter == 0:
+
+    if unknown_counter == 0:  # if all variables are false
         return FALSE
     else:
         if unknown_counter == 1:
-            assign(last_unknown, last_unknown_sign)
+            assign(last_unknown, last_unknown_sign)  # unit propagation
             global var_changed
             var_changed = True
             return TRUE
+
         return UNKNOWN
 
 
 def satisfied():
     finished = True
+
+    remove_pure(clauses)
+
     for clause in clauses:
         it_is_satisfied = clause_satisfied(clause)
+
         if it_is_satisfied == FALSE:
             if resolve_conflict():
-                return satisfied()
+                return satisfied()  # conflict resolved, conflict variable flipped, check again
             else:
                 return FALSE
+
         if it_is_satisfied == UNKNOWN:
             finished = False
+
     if finished:
         return TRUE
     else:
@@ -112,27 +147,20 @@ def check_sat():
     global var_changed
     var_changed = False
     sat = satisfied()
+
     if sat == TRUE:
         result = [TRUE if v == UNKNOWN else v for v in variables]
-        print "v",
+        print ("v"),
         for x in range(nbvar):
-            print ((x + 1) * result[x]),
-        print 0
-        # just to check
-        # for clause in clauses:
-        #     for num in clause:
-        #         if num > 0:
-        #             sign = 1
-        #         else:
-        #             sign = -1
-        #         print result[num * sign - 1] * sign,  # "(", i, ") ",
-        #     print
-        #
-        print "SATISFIABLE"
+            print ((x + 1) * result[x])
+        print (0)
+        print ("SATISFIABLE")
         exit()
+
     if sat == FALSE:
-        print "UNSATISFIABLE"
+        print ("UNSATISFIABLE")
         exit()
+
     if var_changed:
         check_sat()
 
